db6f88fe42958818fd16ec1e81e30810f30970ee
Aprilcuhk <huiqianlaimoney@gmail.com>
2018-12-06 15:33:22 +0800

    first commit

---
+++ app/src/main/java/com/example/huiqian/myapplication/NotificationActivity.java
@@ -0,0 +1,19 @@
+package com.example.huiqian.myapplication;
+
+
+
+import android.app.NotificationManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotificationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notification);
+        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        manager.cancel(1);
+
+    }
+}
